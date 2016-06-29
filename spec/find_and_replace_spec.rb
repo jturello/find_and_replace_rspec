22403c0f180a3755d7aa4da78902e20c46dece5c
James Turello and Matt Wilkin <matt@>
2016-06-29 15:03:49 -0700

    add condition for replacing whole instances of numbers

--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -11,9 +11,12 @@ describe('String#find_and_replace') do
     expect(("Hello world world!").find_and_replace("world", "universe")).to(eq("Hello universe universe!"))
   end
 
-  it('only replaces instances of the whole word world with the word universe') do
+  it('only replaces instances of the whole word cat with the word dog') do
     expect(("I am walking my cat to the cathedral!").find_and_replace("cat", "dog")).to(eq("I am walking my dog to the cathedral!"))
   end
 
+  it('only replaces instances of the whole number 123 with whole number 321') do
+    expect(("123 Testing Road!").find_and_replace("123", "321")).to(eq("321 Testing Road!"))
+  end
 
 end
